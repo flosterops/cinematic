a1c0e970af1fbb837ed624f21c30f790fbebff5b
denysveklych <denys.veklych@guesty.com>
2023-02-16 00:48:30 +0100

    main: Fixed spelling issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
+  },
+  {
+    path: 'registration',
+    component: RegistrationPageComponent,
   },
   {
     path: '',
